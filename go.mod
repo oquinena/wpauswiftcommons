82b9a6bbdeb217d99d24305b005908432909046a
Daniel de Oquiñena <daniel@fast2.se>
2022-03-25 15:54:02 +0100

    Updating dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module wpauswiftcommons
+
+go 1.18
+
+require github.com/ncw/swift/v2 v2.0.1
--- wpauswiftcommons.go
+++ wpauswiftcommons.go
@@ -1,42 +1,44 @@
 package wpauswiftcommons
 
 import (
-    "github.com/ncw/swift"
-    "fmt"
-    "crypto/md5"
-    "io/ioutil"
-    "encoding/hex"
-    "path/filepath"
+	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	swift "github.com/ncw/swift/v2"
 )
 
-func CreatePublicContainer(name string, c swift.Connection) {
-    headers := map[string]string{
-        "X-Container-Read": ".r:*",
-    }
-    c.ContainerCreate(name, headers)
+func CreatePublicContainer(ctx context.Context, name string, c swift.Connection) {
+	headers := map[string]string{
+		"X-Container-Read": ".r:*",
+	}
+	c.ContainerCreate(ctx, name, headers)
 }
 
-func UploadFile(container string, prefix string, path string, c swift.Connection) {
-    dat, err := ioutil.ReadFile(path)
-    if (err != nil) {
-        println(err.Error())
-    } else {
-        name := filepath.Base(path)
-        if len(prefix) > 0 {
-            name = prefix + "-" + name
-        }
-        ext := filepath.Ext(path)
-        hasher := md5.New()
-        hasher.Write(dat)
-        md5hash := hex.EncodeToString(hasher.Sum(nil))
+func UploadFile(ctx context.Context, container string, prefix string, path string, c swift.Connection) {
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		println(err.Error())
+	} else {
+		name := filepath.Base(path)
+		if len(prefix) > 0 {
+			name = prefix + "-" + name
+		}
+		ext := filepath.Ext(path)
+		hasher := md5.New()
+		hasher.Write(dat)
+		md5hash := hex.EncodeToString(hasher.Sum(nil))
 
-        fmt.Printf("Uploading %s to container %s\n", name, container)
-        file, err := c.ObjectCreate(container, name, false, md5hash, ext, nil)
-        if (err != nil) {
-            println(err.Error())
-        } else {
-            file.Write(dat)
-        }
-        file.Close()
-    }
+		fmt.Printf("Uploading %s to container %s\n", name, container)
+		file, err := c.ObjectCreate(container, name, false, md5hash, ext, nil)
+		if err != nil {
+			println(err.Error())
+		} else {
+			file.Write(dat)
+		}
+		file.Close()
+	}
 }
